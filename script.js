6bf7b335815360a59ac2a078c5841f8e81395ed9
SarahCoffland <sadie.coffland@yahoo.com>
2023-05-25 22:18:42 -0700

    fixing gallery, adding content

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const btn = document.querySelectorAll('button');
 btn[0].addEventListener('click', showall);
 btn[1].addEventListener('click', conferencesPublications);
 btn[2].addEventListener('click', trainingsTutorials);
-btn[3].addEventListener('click', communityOutreach);
+btn[3].addEventListener('click', personalProjects);
 
 function showall() {
     const columns = document.getElementById('galleryID').querySelectorAll(".galleryColumn");
@@ -52,14 +52,14 @@ function trainingsTutorials() {
     }
 };
 
-function communityOutreach() {
+function personalProjects() {
     const columns = document.getElementById('galleryID').querySelectorAll(".galleryColumn");
     var arrayLength = columns.length;
     for (var i = 0; i < arrayLength; i++) {
         for(var j=0, len = columns[i].childElementCount; j < len; ++j){
             //columns[i].children[j].style.backgroundColor = "yellow";
             columns[i].children[j].style.display = "block";
-            if (!(columns[i].children[j].classList.contains("co"))) {
+            if (!(columns[i].children[j].classList.contains("pp"))) {
                 columns[i].children[j].style.display = "none";
             }
         }
